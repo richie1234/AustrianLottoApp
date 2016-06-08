9384d2640abddbfb1bede8cdd7e8cc4cd91e00a2
Ruchitha <ruchitha@outlook.com>
2016-06-08 18:13:39 +1000

    LottoApp - added JUnit test

---
+++ src/main/java/app/model/AustrianLotto.java
@@ -0,0 +1,87 @@
+package app.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Ruchitha on 07-Jun-16.
+ */
+public class AustrianLotto {
+
+    public static final int DRAWING_SIZE = 6;
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final int MAX_PICKS = 50;
+
+    public int[] evaluate(String drawing, String[] picks) {
+
+
+
+        if (drawing ==null || picks == null){
+            throw new IllegalArgumentException("input strings and picks cannot be null");
+        }
+        else if (!(picks.length <= MAX_PICKS)) {
+            throw new IllegalArgumentException("Maximum Picks should be 50");
+        }
+
+        int numberOfMatches =0;
+        //used store final results
+        int[] result = new int[7];
+        //split drawing string by spaces
+        String[] drawingTokens = drawing.split(" ");
+        if (drawingTokens.length != DRAWING_SIZE) {
+            throw new IllegalArgumentException("Each drawing should contain six numbers");
+        }
+
+        //Find each pick, split, store, and sorted then
+        for (int i = 0; i < picks.length; i++) {
+            String[] pick = picks[i].split(" ");
+            if (pick.length != DRAWING_SIZE) {
+                throw new IllegalArgumentException("Each pick should contain six numbers");
+            }
+
+            List<Integer> pickList = new ArrayList<Integer>();
+            for (int k = 0; k < pick.length; k++) {
+                int pickedNumber = Integer.parseInt(pick[k].trim());
+                if((MIN_LOTTO_NUMBER <= pickedNumber) &&  (pickedNumber <= MAX_LOTTO_NUMBER)){
+                    pickList.add(pickedNumber);
+                }else {
+                    throw new IllegalArgumentException("Lotto numbers should be between 1 and 45 ");
+                }
+            }
+            Collections.sort(pickList);
+            numberOfMatches = 0;
+            for (int k = 0; k < drawingTokens.length; k++) {
+
+                int searchKey = Integer.parseInt(drawingTokens[k]);
+                if((searchKey < MIN_LOTTO_NUMBER  ) ||  ( MAX_LOTTO_NUMBER < searchKey )){
+                     throw new IllegalArgumentException("Lotto drawing numbers should be between 1 and 45 ");
+                }
+                // Use binary search algorithm to count the matching numbers
+                if (binarySearch(pickList, searchKey)) {
+                    numberOfMatches++;
+                }
+            }
+            //increment the results
+            result[numberOfMatches]++;
+        }
+        return result;
+    }
+
+
+    public static boolean binarySearch(final List<Integer> numbers,
+                                       final Integer key) {
+        int lo = 0;
+        int hi = numbers.size() - 1;
+        while (lo <= hi) {
+            // Key is in a[lo..hi] or not present.
+            int mid = lo + (hi - lo) / 2;
+            if (key < numbers.get(mid)) hi = mid - 1;
+            else if (key > numbers.get(mid)) lo = mid + 1;
+            else return true;
+        }
+        return false;
+    }
+
+}
---
+++ src/test/java/app/model/AustrianLottoUnitTest.java
@@ -0,0 +1,236 @@
+package app.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+/**
+ * Created by Ruchitha on 07-Jun-16.
+ */
+public class AustrianLottoUnitTest {
+
+    private AustrianLotto austrianLotto;
+
+    @Before
+    public void setUp() {
+        austrianLotto = new AustrianLotto();
+
+    }
+
+    @Test
+    public void testLottoAppGivenExampleOne() {
+        String drawing = "3 11 18 23 37 45";
+        String[] picks = {"4 7 14 30 33 35", "1 11 12 25 37 38", "11 18 19 20 21 22"};
+        int[] expectedResult = { 1,  0,  2,  0,  0,  0,  0 };
+        int[] result = austrianLotto.evaluate(drawing ,picks);
+        assertArrayEquals(result,expectedResult);
+    }
+
+    @Test
+    public void testLottoAppAllSixNumbersAreCorrect() {
+        String drawing = "3 18 23 11 37 45";
+        String[] picks = {"3 11 18 23 37 45"};
+        int[] expectedResult = { 0,  0,  0,  0,  0,  0,  1 };
+        int[] result = austrianLotto.evaluate(drawing ,picks);
+        assertArrayEquals(result,expectedResult);
+    }
+
+    @Test
+    public void testLottoAppThreeNumbersAreCorrect() {
+        String drawing = "42 26 33 2 13 14";
+        String[] picks = {"13 4 36 42 26 12"};
+        int[] expectedResult = { 0,  0,  0,  1,  0,  0,  0 };
+        int[] result = austrianLotto.evaluate(drawing ,picks);
+        assertArrayEquals(result,expectedResult);
+    }
+
+
+    @Test
+    public void testLottoAppDrawingDoesNotMatchAnyPicks() {
+        String drawing = "3 11 18 23 37 45";
+        String[] picks = {"4 7 14 30 33 35", "1 14 12 25 35 38", "19 13 19 20 21 22"};
+        int[] expectedResult = { 3,  0,  0,  0,  0,  0,  0 };
+        int[] result = austrianLotto.evaluate(drawing ,picks);
+        assertArrayEquals(result,expectedResult);
+    }
+
+    @Test
+    public void testLottoAppDrawingMatchOnlyOneNumberInSecondPick() {
+        String drawing = "3 11 18 23 37 45";
+        String[] picks = {"4 7 14 30 33 35", "1 11 12 25 35 38", "19 13 19 20 21 22"};
+        int[] expectedResult = { 2,  1,  0,  0,  0,  0,  0 };
+        int[] result = austrianLotto.evaluate(drawing ,picks);
+        assertArrayEquals(result,expectedResult);
+    }
+
+     @Test
+    public void testLottoAppThreeNumbersCorrectAndAllSixNumbersCorrect() {
+        String drawing = "3 11 18 23 37 45";
+        String[] picks = {"4 7 14 30 33 35", "1 14 12 25 35 38","11 3 45 23 37 18",
+                "19 13 19 20 21 22", "45 11 19 20 3 22"};
+        int[] expectedResult = { 3,  0,  0,  1,  0,  0,  1 };
+        int[] result = austrianLotto.evaluate(drawing ,picks);
+        assertArrayEquals(result,expectedResult);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testLottoAppDrawingMoreThanSixNumbers() {
+        String drawing = "3 11 18 23 37 45 34";
+        String[] picks = {"13 4 36 42 26 12"};
+        int[] expectedResult = { 0,  0,  0,  1,  0,  0,  0 };
+        int[] result = austrianLotto.evaluate(drawing ,picks);
+        assertArrayEquals(result,expectedResult);
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testLottoAppDrawingLessThanSixNumbers() {
+        String drawing = "42 26 33 2 13";
+        String[] picks = {"13 4 36 42 26 12"};
+        int[] expectedResult = { 0,  0,  0,  1,  0,  0,  0 };
+        int[] result = austrianLotto.evaluate(drawing ,picks);
+        assertArrayEquals(result,expectedResult);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testLottoAppPickLessThanSixNumbers() {
+        String drawing = "3 11 18 23 37 45";
+        String[] picks = {"13 4 36 42 26"};
+        int[] expectedResult = { 0,  0,  0,  1,  0,  0,  0 };
+        int[] result = austrianLotto.evaluate(drawing ,picks);
+        assertArrayEquals(result,expectedResult);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testLottoAppPickMoreThanSixNumbers() {
+        String drawing = "42 26 33 2 13 14";
+        String[] picks = {"13 4 36 42 26 12 20"};
+        int[] expectedResult = { 0,  0,  0,  1,  0,  0,  0 };
+        int[] result = austrianLotto.evaluate(drawing ,picks);
+        assertArrayEquals(result,expectedResult);
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testLottoAppLottoPickNumbersShouldBetweenOneAndFortyFive() {
+        String drawing = "42 26 33 2 13 14";
+        String[] picks = {"13 4 36 42 26 -12"};
+        int[] expectedResult = { 0,  0,  0,  1,  0,  0,  0 };
+        int[] result = austrianLotto.evaluate(drawing ,picks);
+        assertArrayEquals(result,expectedResult);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testLottoAppDrawingNumbersShouldBetweenOneAndFortyFive() {
+        String drawing = "42 26 33 2 -13 14";
+        String[] picks = {"13 4 36 42 26 12"};
+        int[] expectedResult = { 0,  0,  0,  1,  0,  0,  0 };
+        int[] result = austrianLotto.evaluate(drawing ,picks);
+        assertArrayEquals(result,expectedResult);
+    }
+
+// Boundy value testing
+    @Test(expected = IllegalArgumentException.class)
+    public void testLottoAppDrawingTestForZero() {
+        String drawing = "42 26 33 2 13 0";
+        String[] picks = {"13 4 36 42 26 12"};
+        int[] expectedResult = { 0,  0,  0,  1,  0,  0,  0 };
+        int[] result = austrianLotto.evaluate(drawing ,picks);
+        assertArrayEquals(result,expectedResult);
+    }
+
+    // Boundy value testing
+    @Test(expected = IllegalArgumentException.class)
+    public void testLottoAppDrawingTestForFortySIX() {
+        String drawing = "42 26 33 2 46 12";
+        String[] picks = {"13 4 36 42 26 12"};
+        int[] expectedResult = { 0,  0,  0,  1,  0,  0,  0 };
+        int[] result = austrianLotto.evaluate(drawing ,picks);
+        assertArrayEquals(result,expectedResult);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testLottoAppPicksTestForZero() {
+        String drawing = "42 26 33 2 13 12";
+        String[] picks = {"13 4 36 0 26 12"};
+        int[] expectedResult = { 0,  0,  0,  1,  0,  0,  0 };
+        int[] result = austrianLotto.evaluate(drawing ,picks);
+        assertArrayEquals(result,expectedResult);
+    }
+
+    // Boundy value testing
+    @Test(expected = IllegalArgumentException.class)
+    public void testLottoAppPicksTestForFortySIX() {
+        String drawing = "42 26 33 2 45 12";
+        String[] picks = {"13 4 36 42 46 12"};
+        int[] expectedResult = { 0,  0,  0,  1,  0,  0,  0 };
+        int[] result = austrianLotto.evaluate(drawing ,picks);
+        assertArrayEquals(result,expectedResult);
+    }
+
+    // Boundy value testing
+    @Test
+    public void testLottoAppChackForMaxPicksFifty() {
+        String drawing = "42 26 33 2 45 12";
+        String[] picks = new String[50];
+        for (int i =0; i<50;i++ ){
+            picks[i] = "13 4 36 42 8 12";
+        }
+        int[] expectedResult = { 0,  0,  50,  0,  0,  0,  0 };
+        int[] result = austrianLotto.evaluate(drawing ,picks);
+        assertArrayEquals(result,expectedResult);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testLottoAppChackForMaxPicksFiftyOne() {
+        String drawing = "42 26 33 2 45 12";
+        String[] picks = new String[51];
+        for (int i =0; i<51;i++ ){
+            picks[i] = "13 4 36 42 8 12";
+        }
+        int[] expectedResult = { 0,  0,  51,  0,  0,  0,  0 };
+        int[] result = austrianLotto.evaluate(drawing ,picks);
+        assertArrayEquals(result,expectedResult);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testLottoAppDrawingEmpty() {
+        String drawing = "";
+        String[] picks = new String[51];
+        for (int i =0; i<51;i++ ){
+            picks[i] = "13 4 36 42 8 12";
+        }
+        int[] expectedResult = { 0,  0,  51,  0,  0,  0,  0 };
+        int[] result = austrianLotto.evaluate(drawing ,picks);
+        assertArrayEquals(result,expectedResult);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testLottoAppOnePickEmpty() {
+        String drawing = "3 11 18 23 37 45";
+        String[] picks = {"", "1 14 12 25 35 38", "19 13 19 20 21 22"};
+        int[] expectedResult = { 3,  0,  0,  0,  0,  0,  0 };
+        int[] result = austrianLotto.evaluate(drawing ,picks);
+        assertArrayEquals(result,expectedResult);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testLottoAppDrawingIsNull() {
+        String drawing = null;
+        String[] picks = {"1 14 12 25 35 38", "19 13 19 20 21 22"};
+        int[] expectedResult = { 3,  0,  0,  0,  0,  0,  0 };
+        int[] result = austrianLotto.evaluate(drawing ,picks);
+        assertArrayEquals(result,expectedResult);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testLottoAppPicksAreEmpty() {
+        String drawing = null;
+        String[] picks = {};
+        int[] expectedResult = { 3,  0,  0,  0,  0,  0,  0 };
+        int[] result = austrianLotto.evaluate(drawing ,picks);
+        assertArrayEquals(result,expectedResult);
+    }
+
+}
